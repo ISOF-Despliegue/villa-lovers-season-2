8905da76f748597b4741c6c697acd74dc7db3bca
Miguel Eduardo Escobar Ladron De Guevara <miguelguevara850523@gmail.com>
2026-02-20 10:30:47 -0600

    funcionalidad de sumas y restas

--- src/index.js
+++ src/index.js
@@ -5,8 +5,33 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Servidor funcionando 🚀');
+function validarNumeros(num1, num2) {
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
+    if (isNaN(numero1) || isNaN(numero2)) {
+        return { error: 'Los valores deben ser números' };
+    }
+
+    return { numero1, numero2 };
+}
+
+// SUMA
+app.get('/suma', (req, res) => {
+    const { numero1, numero2, error } = validarNumeros(req.query.num1, req.query.num2);
+
+    if (error) return res.status(400).json({ error });
+
+    res.json({ resultado: numero1 + numero2 });
+});
+
+// RESTA
+app.get('/resta', (req, res) => {
+    const { numero1, numero2, error } = validarNumeros(req.query.num1, req.query.num2);
+
+    if (error) return res.status(400).json({ error });
+
+    res.json({ resultado: numero1 - numero2 });
 });
 
 app.listen(port, () => {
