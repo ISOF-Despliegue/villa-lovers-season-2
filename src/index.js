d423554856daa43b627cc6be6a539982e710aba2
Miguel Eduardo Escobar Ladron De Guevara <miguelguevara850523@gmail.com>
2026-02-20 10:15:23 -0600

    primera parte de la practica de la calculadora

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando 🚀');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
