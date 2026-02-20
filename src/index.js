9e831fbfe11bf7f0f43ed5e2dca59a5be884b129
Uriel Cendon Diaz <urielcd2013@gmail.com>
2026-02-20 10:42:10 -0600

    Validar que sean exactamente 2 números y multiplicación

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-function validarNumeros(num1, num2) {
-    const numero1 = parseFloat(num1);
-    const numero2 = parseFloat(num2);
+function validarNumeros(query) {
+    const keys = Object.keys(query);
+
+    if (keys.length !== 2) {
+        return { error: 'La operación permite exactamente 2 números (num1 y num2)' };
+    }
+
+    if (!query.num1 || !query.num2) {
+        return { error: 'Debes proporcionar num1 y num2' };
+    }
+
+    const numero1 = parseFloat(query.num1);
+    const numero2 = parseFloat(query.num2);
 
     if (isNaN(numero1) || isNaN(numero2)) {
-        return { error: 'Los valores deben ser números' };
+        return { error: 'Los valores proporcionados deben ser números' };
     }
 
     return { numero1, numero2 };
@@ -18,7 +28,7 @@ function validarNumeros(num1, num2) {
 
 // SUMA
 app.get('/suma', (req, res) => {
-    const { numero1, numero2, error } = validarNumeros(req.query.num1, req.query.num2);
+    const { numero1, numero2, error } = validarNumeros(req.query);
 
     if (error) return res.status(400).json({ error });
 
@@ -27,11 +37,20 @@ app.get('/suma', (req, res) => {
 
 // RESTA
 app.get('/resta', (req, res) => {
-    const { numero1, numero2, error } = validarNumeros(req.query.num1, req.query.num2);
+    const { numero1, numero2, error } = validarNumeros(req.query);
 
     if (error) return res.status(400).json({ error });
 
     res.json({ resultado: numero1 - numero2 });
+});
+
+// MULTIPLICACION
+app.get('/multiplicacion', (req, res) => {
+    const { numero1, numero2, error } = validarNumeros(req.query);
+
+    if (error) return res.status(400).json({ error });
+
+    res.json({ resultado: numero1 * numero2 });
 });
 
 app.listen(port, () => {
